a5d629225ed407d8775a9b8836f8bf249bdf63b9
TJ Heidrick <tjheidrick@hotmail.com>
2022-03-05 09:25:41 -0700

    changes for v2.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from TJ in V2</h1>
+        <h1>Hello from Dad/TJ in V2</h1>
       </header>
     </div>
   );
